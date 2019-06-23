941d92b59d8322d1a02288c08a93e61bc43ae4f4
jesus-son-coder <rvck2007@gmail.com>
2019-06-23 23:13:42 +0200

    Project 0

---
+++ 01_base_HTML5.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <!-- Librairies de Style -->
+  <!-- ------------------- -->
+  <link rel="stylesheet" href="./css/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/main.css">
+
+</head>
+<body>
+  ...
+  <!-- Le reste du contenu -->
+  ...
+
+  <!-- Librairies JavaScrip -->
+  <!-- -------------------- -->
+  <script src="./js/jquery/jquery_3_3_1.js"></script>
+  <script src="./js/bootstrap/bootstrap.min.js"></script>
+</body>
+</html>
